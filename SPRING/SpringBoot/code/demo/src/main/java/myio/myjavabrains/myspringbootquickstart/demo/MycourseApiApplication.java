57b22512935fdd46aea6c2a4adea6d07efc46e53
Mohd Yasin Ansari <mohammad.yasin006@gmail.com>
2018-06-01 11:57:58 +0530

    01-06-2018

---
+++ SPRING/SpringBoot/code/demo/src/main/java/myio/myjavabrains/myspringbootquickstart/demo/MycourseApiApplication.java
@@ -0,0 +1,12 @@
+package myio.myjavabrains.myspringbootquickstart.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycourseApiApplication.class, args);
+	}
+}
